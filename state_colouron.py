687133328a1153811a8647e9126a1c8e340fd937
Twinkle25 <108237575+devdacoder@users.noreply.github.com>
2026-07-23 13:06:58 +0530

    Update state_colouron.py

--- state_colouron.py
+++ state_colouron.py
@@ -775,7 +775,7 @@ for _, row in merged.iterrows():
             "Chhattisgarh":-0.14,
             "Rajasthan":-0.12,
             "Andhra Pradesh":-0.14,
-            "Telangana":-0.14
+            "Telangana":-0.11
         }
         # Get the offset for the current state, default to -0.1 if not found
         current_offset = state_y_offsets.get(target_state, -0.1)
