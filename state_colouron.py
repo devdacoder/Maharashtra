b94855cd32b6ce43adad1cdb79a5f7f51fd69f2c
Twinkle25 <108237575+devdacoder@users.noreply.github.com>
2026-07-24 12:42:13 +0530

    Update state_colouron.py

--- state_colouron.py
+++ state_colouron.py
@@ -364,7 +364,7 @@ def get_geojson(state_name):
         'CHIKKABALL': 'CHIKKABALLAPURA',
         'CHIKKAMAGAL#RU': 'CHIKKAMAGALURU',
         'D': 'DAVANGERE',
-        'Dh': 'DHARWAD',
+        'DH': 'DHARWAD',
         'H': 'HASSAN',
         'H': 'HAVERI',
         'KOL': 'KOLAR',
@@ -834,7 +834,7 @@ for _, row in merged.iterrows():
             "Rajasthan":-0.12,
             "Andhra Pradesh":-0.14,
             "Telangana":-0.05,
-            "Karnataka":-0.14
+            "Karnataka":-0.15
         }
         # Get the offset for the current state, default to -0.1 if not found
         current_offset = state_y_offsets.get(target_state, -0.1)
