ee7dbf0dec4c1017192b86a3702a16bc249dab35
Twinkle25 <108237575+devdacoder@users.noreply.github.com>
2026-09-18 15:42:36 +0530

    Update state_colouron.py

--- state_colouron.py
+++ state_colouron.py
@@ -1374,7 +1374,7 @@ if target_state == "Assam":
 
     # Sikkim
     annotations.append(dict(
-        x=89.9, y=27.5, text="<b>SIKKIM</b>",
+        x=89.3, y=27.5, text="<b>SIKKIM</b>",
         showarrow=False,
         font=dict(size=11, color="black", family="Arial Black"),
         xref="x", yref="y"
@@ -1382,7 +1382,7 @@ if target_state == "Assam":
 
     # Arunachal Pradesh
     annotations.append(dict(
-        x=92.5, y=28.5, text="<b>ARUNACHAL PRADESH</b>",
+        x=92.1, y=28.5, text="<b>ARUNACHAL PRADESH</b>",
         showarrow=False,
         font=dict(size=11, color="black", family="Arial Black"),
         xref="x", yref="y"
@@ -1406,7 +1406,7 @@ if target_state == "Assam":
 
     # Mizoram
     annotations.append(dict(
-        x=93.9, y=23.7, text="<b>MIZORAM</b>",
+        x=93.9, y=23.4, text="<b>MIZORAM</b>",
         showarrow=False,
         font=dict(size=11, color="black", family="Arial Black"),
         xref="x", yref="y"
@@ -1422,7 +1422,7 @@ if target_state == "Assam":
 
     # Tripura
     annotations.append(dict(
-        x=90.8, y=24.0, text="<b>TRIPURA</b>",
+        x=90.7, y=24.0, text="<b>TRIPURA</b>",
         showarrow=False,
         font=dict(size=11, color="black", family="Arial Black"),
         xref="x", yref="y"
