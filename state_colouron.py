4741ddf08828e27c76821137e4a9cf659a629d9b
Twinkle25 <108237575+devdacoder@users.noreply.github.com>
2026-09-18 16:59:08 +0530

    Update state_colouron.py

--- state_colouron.py
+++ state_colouron.py
@@ -1367,7 +1367,7 @@ else:
                 "Assam":-0.06,
                 "Odisha":-0.1,
                 "West Bengal":-0.1,
-                "Jharkhand":-0.1
+                "Jharkhand":-0.05
             }
             # Get the offset for the current state, default to -0.1 if not found
             current_offset = state_y_offsets.get(target_state, -0.1)
@@ -1508,7 +1508,7 @@ if target_state == "West Bengal":
     ))
 if target_state == "Jharkhand":
     annotations.append(dict(
-        x=86.2, y=22.7, text="<b>SINGHBHUM</b>",
+        x=87, y=22.7, text="<b>SINGHBHUM</b>",
         showarrow=False,
         font=dict(size=13, color="black", family="Arial Black"),
         xref="x", yref="y"
